ced3ece9ad90e061c05bde7934c935c28dd7efb7
Dev-Srivastava12 <123351711+Dev-Srivastava12@users.noreply.github.com>
2023-11-23 08:22:55 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,31 @@
+let number = null;
+
+let num = Number(number);
+//console.log(typeof num);
+//console.log(num);
+
+//"33"=>33
+//"33abc" => NaN(type of NaN is number)
+//null => 0
+//undefined => NaN
+
+let bool = undefined;
+
+let isValue = Boolean(bool);
+//console.log(typeof isValue);
+//console.log(isValue);
+
+//1=>true 0=>false any number=>true
+//""=>false  "anystring"=>true
+//null=>false
+//undefined=>false
+
+let str = undefined;
+
+let str1 = String(str);
+console.log(typeof str1);
+console.log(str1);
+
+//33=>"33"
+//null=>null
+//undefined=>undefined
