e4e3c00159088dfa5a5112ca1477390a656aa72f
Dev-Srivastava12 <123351711+Dev-Srivastava12@users.noreply.github.com>
2023-11-26 08:38:46 +0000

    concepts of array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,32 @@
+let myArrr = [1,2,3,4,5,6]
+// let newArr = [1,false,"abc",4,"tt"]
+
+// console.log(newArr);
+
+// myArrr.push(7);  //add element at last
+// console.log(myArrr); 
+
+// myArrr.pop() //return array by removing last element
+// console.log(myArrr);
+
+//  myArrr.unshift(7); //add element in an array in starting
+// // console.log(myArrr);
+
+// myArrr.shift()   //remove the starting element
+// console.log(myArrr);
+
+
+let xyz = [1,2,3,4,5,6]
+
+console.log("A",xyz);
+
+console.log(xyz.slice(1,3));  //slice return a part of array and doen't effect actual array
+
+console.log("B",xyz)
+
+console.log(xyz.splice(1,3));//splice return a part of array and effect the actual array
+
+console.log(xyz)
+
+
+
